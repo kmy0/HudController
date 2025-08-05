61cb7d4752622a302937f343899884217c7a1927
kmy0 <93156259+kmy0@users.noreply.github.com>
2025-08-05 17:32:05 +0200

    cache all_type

--- reframework/autorun/HudController/hud/play_object/child.lua
+++ reframework/autorun/HudController/hud/play_object/child.lua
@@ -1,7 +1,9 @@
+local cache = require("HudController.util.misc.cache")
 local control = require("HudController.hud.play_object.control")
 local util_table = require("HudController.util.misc.table")
 
 local this = {}
+local all_cache = cache:new()
 
 ---@generic T
 ---@param ctrl via.gui.Control
@@ -26,18 +28,44 @@ end
 function this.all_type(ctrl, regex, child_type)
     local ret = {}
 
-    local child = ctrl:get_Child()
-    while child do
-        local type = child:get_type_definition() --[[@as RETypeDefinition]]
+    ---@type [via.gui.Control, string[], string, string][]?, string
+    local cached, key = all_cache:get_hashed(false, ctrl, regex, child_type)
 
-        if type:is_a(child_type) and (not regex or regex and string.match(child:get_Name(), regex)) then
-            table.insert(ret, child)
-        elseif type:is_a("via.gui.Control") then
-            ---@cast child via.gui.Control
-            util_table.array_merge_t(ret, this.all_type(child, regex, child_type))
+    if cached then
+        for _, arg in pairs(cached) do
+            local candidate = this.get(table.unpack(arg))
+            if candidate then
+                table.insert(ret, candidate)
+            end
+        end
+    else
+        local args = {}
+
+        ---@param ctrl via.gui.Control
+        ---@param arg [via.gui.Control, string[], string, string]
+        local function iter(ctrl, arg)
+            local child = ctrl:get_Child()
+            while child do
+                local type = child:get_type_definition() --[[@as RETypeDefinition]]
+
+                if type:is_a(child_type) and (not regex or regex and string.match(child:get_Name(), regex)) then
+                    table.insert(ret, child)
+                    local _arg = util_table.deep_copy(arg)
+                    _arg[3] = child:get_Name()
+                    table.insert(args, _arg)
+                elseif type:is_a("via.gui.Control") then
+                    ---@cast child via.gui.Control
+                    local _arg = util_table.deep_copy(arg)
+                    table.insert(_arg[2], child:get_Name())
+                    iter(child, _arg)
+                end
+
+                child = child:get_Next()
+            end
         end
 
-        child = child:get_Next()
+        iter(ctrl, { ctrl, {}, "placeholder", child_type })
+        all_cache:set(key, args)
     end
 
     return ret
