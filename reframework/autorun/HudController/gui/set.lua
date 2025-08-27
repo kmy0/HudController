88164103cdd4309766faf633ac6ff7b2daaec7f7
kmy0 <93156259+kmy0@users.noreply.github.com>
2025-08-27 07:34:09 +0200

    slider_float -> slider_float_step

--- reframework/autorun/HudController/gui/set.lua
+++ reframework/autorun/HudController/gui/set.lua
@@ -1,4 +1,5 @@
 local config = require("HudController.config")
+local util_misc = require("HudController.util.misc")
 
 local this = {}
 
@@ -41,6 +42,26 @@ end
 ---@return boolean
 function this.slider_float(name, config_key, ...)
     return this.generic_config(name, config_key, imgui.slider_float, ...)
+end
+
+---@param name string
+---@param config_key string
+---@param v_min integer
+---@param v_max integer
+---@param step number
+---@param display_format string?
+---@return boolean
+function this.slider_float_step(name, config_key, v_min, v_max, step, display_format)
+    local changed, value
+    v_max = v_max / step
+    v_min = v_min / step
+    local v = config:get(config_key) / step --[[@as number]]
+
+    changed, value = imgui.slider_int(name, v, v_min, v_max, display_format)
+    if changed then
+        config:set(config_key, util_misc.round(value * step, util_misc.get_decimals(step)))
+    end
+    return changed
 end
 
 ---@param name string
