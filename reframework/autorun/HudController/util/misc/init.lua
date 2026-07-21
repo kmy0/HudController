f8d72ab1ca4165dab78e7b2aefd71a606ba0c397
kmy0 <kmygat@gmail.com>
2026-07-21 13:38:08 +0200

    remove lru util

--- reframework/autorun/HudController/util/misc/init.lua
+++ reframework/autorun/HudController/util/misc/init.lua
@@ -2,7 +2,6 @@ local this = require("HudController.util.misc.util")
 
 this.hash = require("HudController.util.misc.hash")
 this.init_chain = require("HudController.util.misc.init_chain")
-this.lru = require("HudController.util.misc.lru")
 this.table = require("HudController.util.misc.table")
 this.timer = require("HudController.util.misc.timer")
 this.uuid = require("HudController.util.misc.uuid")
