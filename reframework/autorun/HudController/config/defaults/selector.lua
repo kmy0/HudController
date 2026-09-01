f90315e516d21346938beaeace45d00d941cd9a6
kmy0 <kmygat@gmail.com>
2026-09-01 18:47:04 +0200

    fix backup combo index

--- reframework/autorun/HudController/config/defaults/selector.lua
+++ reframework/autorun/HudController/config/defaults/selector.lua
@@ -1,9 +1,11 @@
 ---@class (exact) SelectorSettings : SettingsBase
 ---@field combo_file integer
+---@field combo_file_backup integer
 ---@field file string
 
 ---@type SelectorSettings
 return {
     combo_file = 1,
+    combo_file_backup = 1,
     file = "config.json",
 }
