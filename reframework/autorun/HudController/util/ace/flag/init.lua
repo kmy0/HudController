0d0047fcd28f6d4ac9b38c2deb6224150320db3e
kmy0 <kmygat@gmail.com>
2026-09-04 11:38:13 +0200

    remove ace_flag

--- reframework/autorun/HudController/util/ace/flag/init.lua
+++
@@ -1,8 +0,0 @@
----@alias AceFlags ace.cSafeContinueFlagGroup | ace.BIT_FLAG | ace.cSafeContinueFlag
-
-local this = {
-    setter = require("HudController.util.ace.flag.setter"),
-    checker = require("HudController.util.ace.flag.checker"),
-}
-
-return this
